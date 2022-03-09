d0c2e28c608e0bd30d081d7c27c2eff6b3261a1d
kimmiran <shortyjjang8484@gmail.com>
2022-03-09 20:02:18 +0900

    로그인

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -29,6 +32,48 @@ const userSchema = mongoose.Schema({
         type: Number,
     }
 });
+
+//회원정보 저장
+userSchema.pre('save',function(next){
+    const user = this;
+
+    //비밀번호 변경시에만 활성화
+    if(user.isModified('password')){
+        //비밀번호 암호화
+        bcrypt.genSalt(saltRounds,function(err, salt){
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash
+                next();
+            })
+        })
+    }else{
+        next();
+    }
+
+})
+
+//비밀번호비교
+userSchema.methods.comparePassword = function(pw,callback) {
+    bcrypt.compare(pw, this.password, function(err, isMatch){
+        if(err) return callback(err),
+        callback(null, isMatch)
+    })
+}
+
+//토큰생성
+userSchema.methods.generateToken = function(callback) {
+    const user = this;
+    const token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return callback(err)
+        callback(null, user)
+    })
+}
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ const app = express()
 const port = 5000;
 const { User } = require('./models/User');
 
-const config = require('./config/key')
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+const cookieParser = require('cookie-parser')
+app.use(cookieParser)
+
 const mongoose = require('mongoose');
+const config = require('./config/key')
 mongoose.connect(config.mongoURI, { 
   useNewUrlParser: true, useUnifiedTopology: true
 })
@@ -19,14 +21,45 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => {
   res.send('Hello Sever!!')
 })
-app.post('/register', (req, res) =>{
 
-  //회원가입에 대한 정보를 데이터베이스에 저장
+//회원가입에 대한 정보를 데이터베이스에 저장
+app.post('/register', (req, res) =>{
   const user = new User(req.body)
   user.save((err,doc) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({success: true})
   });
+})
+
+//회원가입에 대한 정보를 데이터베이스에 저장
+app.post('/login', (req, res) =>{
+  //이메일 데이터베이스에서 찾기
+  User.findOne({email: req.body.email},(err,user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 사용자가 없습니다."
+      })
+    }
+    
+    //비밀번호 비교
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+      //비밀번호 일치시 토큰생성
+      user.generateToken((err,user) => {
+        if(err) return res.status(400).send(err);
+
+        //토큰을 저장
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId: user._id})
+      })
+    })
+
+  })
+
+
 })
 
 
